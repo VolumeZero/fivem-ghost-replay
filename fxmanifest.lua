36367c94f1bd829807a33b9dcb99ffbe73ce5bac
Gavino <gavinkilfoy@hotmail.com>
2025-12-14 18:57:56 -0800

    update author in manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 
 name 'ghost_replay'
 description 'Simple ghost car recorder / replay for FiveM (Lua)'
-author 'Converted for FiveM'
+author 'VolumeZero'
 version '1.0.0'
 
 server_script 'server/main.lua'
